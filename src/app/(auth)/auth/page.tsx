a3d3f9fd4262c11814416ec5d4a6514f0cd9d219
BoltUltra <mayowa.u.sunusi@gmail.com>
2025-04-21 19:29:14 +0100

    App Setup

---
+++ src/app/(auth)/auth/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <p>Authentication page</p>
+      <Link href="/">Go back home</Link>
+    </div>
+  );
+};
+
+export default page;
---
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <p>Dashboard</p>
+      <Link href="/">Go back home</Link>
+    </div>
+  );
+};
+
+export default page;

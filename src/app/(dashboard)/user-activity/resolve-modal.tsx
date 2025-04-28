9a1c7bf7f572d2526f7f53f700182112baf377f5
Akinshola <114211385+Akshola00@users.noreply.github.com>
2025-04-28 02:15:57 +0000

    Add new avatar and logo images
    - Added avatar-one.png with a size of 9651 bytes.
    - Added strk-logo.png with a size of 5417 bytes.

--- src/app/(dashboard)/user-activity/resolve-modal.tsx
+++ src/app/(dashboard)/user-activity/resolve-modal.tsx
@@ -2,6 +2,8 @@ import { Copy, LogOut, X } from 'lucide-react';
 import React from 'react';
 import { Inter } from 'next/font/google';
 import Image from 'next/image';
+import avatarOne from "../../../../public/avatar-one.png"
+import strkLogo from "../../../../public/strk-logo.png"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,8 +13,8 @@ const ResolveDisputeModal: React.FC = ({ }) => {
         <div className={`fixed inset-0 z-50 flex items-center justify-center ${inter.className} `}>
             <div className="absolute inset-0 bg-black bg-opacity-70"></div>
 
-            <div className='relative bg-white rounded-lg w-full h-full overflow-y-auto grid justify-items-end'>
-                <div className='w-[582px] rounded-[24px] m-4 p-6 bg-blue-300 flex flex-col'>
+            <div className='relative bg- rounded-lg w-full h-full overflow-y-auto grid justify-items-end'>
+                <div className='w-[582px] rounded-[24px] m-4 p-6 bg-[#EFF1F5] flex flex-col'>
                     <div className='flex items-center justify-between w-full'>
                         <p className="font-semibold text-[32px] text-[#101828] ">Dispute Details</p>
                         <X width={24} />
@@ -49,51 +51,80 @@ const ResolveDisputeModal: React.FC = ({ }) => {
                         </div>
                         <div className='flex gap-2 flex-col'>
                             <p className='text-[#7D89B0]'>Participants</p>
-                            <div className='grid grid-cols-2 gap-4'>
-                                <div>
-                                    <Image src="" alt="" />
+                            <div className='grid grid-cols-2 gap-4 text-[14px]'>
+                                <div className='w-[253] h-[211px] rounded-[16px] p-4 bg-[#FFFFFF] flex flex-col gap-2'>
+                                    <Image width={56} height={56} src={avatarOne} className='rounded-[12px]' alt="avatar_img" />
                                     <p>Username: <span>@noyi24_7</span></p>
-                                    <p>Wallet Address: <span>0x4040...7b</span> <Copy /> </p>
+                                    <p className='flex gap-1'>
+                                        Wallet Address: 
+                                        <span id="wallet-address">0x4040...7b</span> 
+                                        <Copy 
+                                            width={14} 
+                                            onClick={() => {
+                                                const walletAddress = document.getElementById('wallet-address')?.textContent;
+                                                if (walletAddress) {
+                                                    navigator.clipboard.writeText(walletAddress);
+                                                    alert('Wallet address copied to clipboard!');
+                                                }
+                                            }} 
+                                            className='cursor-pointer'
+                                        /> 
+                                    </p>
                                     <p>Wagers Created: <span>25</span></p>
                                     <p>Wagers Joined: <span>74</span></p>
                                 </div>
-                                <div>
-                                    <Image src="" alt="" />
+                                <div className='w-[253] h-[211px] rounded-[16px] p-4 bg-[#FFFFFF] flex flex-col gap-2'>
+                                    <Image width={56} height={56} src={avatarOne} className='rounded-[12px]' alt="avatar_img" />
                                     <p>Username: <span>@noyi24_7</span></p>
-                                    <p>Wallet Address: <span>0x4040...7b</span> <Copy /> </p>
+                                    <p className='flex gap-1'>
+                                        Wallet Address: 
+                                        <span id="wallet-address">0x4040...7b</span> 
+                                        <Copy 
+                                            width={14} 
+                                            onClick={() => {
+                                                const walletAddress = document.getElementById('wallet-address')?.textContent;
+                                                if (walletAddress) {
+                                                    navigator.clipboard.writeText(walletAddress);
+                                                    alert('Wallet address copied to clipboard!');
+                                                }
+                                            }} 
+                                            className='cursor-pointer'
+                                        /> 
+                                    </p>
                                     <p>Wagers Created: <span>25</span></p>
                                     <p>Wagers Joined: <span>74</span></p>
                                 </div>
                             </div>
                         </div>
-                        <div>
-                            <p>Amount Staked</p>
-                            <div>
-                                <LogOut />
+                        <div className='flex gap-2 flex-col'>
+                            <p className='text-[#7D89B0]'>Amount Staked</p>
+                            <div className='flex bg-[#FFFFFF] rounded-full p-2 gap-2 w-fit'>
+                                <Image width={16} height={16} src={strkLogo} alt="strk logo" />
 
-                                <p>10 strk</p>
+                                <p>10 Strk</p>
                             </div>
                         </div>
-                        <div>
-                            <p>Reasin for Dispute</p>
+                        <div className='flex gap-2 flex-col'>
+                            <p className='text-[#7D89B0]'>Reason for Dispute</p>
                             <p> No proof provided for claim</p>
                         </div>
-                        <div>
-                            <p>Category</p>
-                            <span>Crypto</span>
+                        <div className='flex gap-2 flex-col'>
+                            <p className='text-[#7D89B0]'>Category</p>
+                            <span className='bg-white p-2 w-fit rounded-full'>Crypto</span>
                         </div>
-                        <div>
-                            <p>Proof</p>
-                            <Image src="" alt="" />
+                        <div className='flex gap-2 flex-col'>
+                            <p className='text-[#7D89B0]'>Proof</p>
+                            <Image width={120} className='rounded-[12px] border' src="" alt="proof images" />
                         </div>
-                        <div>
-                            <div>Resolve Dispute</div>
-                            <div>Request More Info</div>
-                            <div>Close</div>
+
+                        <div className='flex flex-row gap-2'>
+                            <div className='w-[211px] h-[56px] rounded-[16px] p-3 bg-[#E0FE10] flex items-center justify-center border border-[#DCDFEA]'>Resolve Dispute</div>
+                            <div className='w-[211px] h-[56px] rounded-[16px] p-3 flex items-center justify-center border bg-[#F9F9FB] border-[#DCDFEA]'>Request More Info</div>
+                            <div className='w-[80px] h-[56px] rounded-[16px] p-3 flex items-center justify-center border bg-[#F9F9FB] border-[#DCDFEA]'>Close</div>
                         </div>
                         <div>
                             <p>Reason for Resolution (Optional) </p>
-                            <input type="text" placeholder='Type here...' className='w-full h-[40px] border border-[#D0D5DD] rounded-[8px] p-2' />
+                            <input type="text" placeholder='Tell them why ?' className='w-full h-[40px] border border-[#D0D5DD] rounded-[8px] p-2 bg-[#F9F9FB]' />
                             <p>0/500</p>
                         </div>
                         <div>
